c7c0425956d08f89f3553f6ba0f670757f3eef8e
Abooof <Abdullah.abooof@gmail.com>
2024-05-20 23:37:05 +0300

    refactor: Remove unused code for removing items from the order page

--- src/app/order/page.jsx
+++ src/app/order/page.jsx
@@ -66,14 +66,7 @@ const Order = () => {
                                         <div className="flex flex-1 items-end justify-between text-sm">
                                             <p className="text-gray-500">Qty {product.quantity}</p>
 
-                                            <div className="flex">
-                                                <button
-                                                    type="button"
-                                                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                                                >
-                                                    Remove
-                                                </button>
-                                            </div>
+                                            
                                         </div>
                                     </div>
                                 </li>
