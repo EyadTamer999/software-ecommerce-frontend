6105ac74c1ff8486485051cf5e43a76d21e3d008
EyadTamer999 <eyadhassan2002@gmail.com>
2024-05-24 14:40:03 +0300

    refactor: Update Home page to use new ProductList component and improve image URLs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,41 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import ProductList from './product/ProductList'
-import { getAllProducts } from './product/fetchApi';
+import { getAllProducts, getPopularProducts, getTopOffers } from './product/fetchApi';
 
 export default function Home() {
 
   useEffect(() => {
     getAllProducts().then((response) => {
       console.log(response);
-      setProducts(response.data);
+      setProducts(response.data.slice(0, 8));
+    })
+
+    getTopOffers().then((response) => {
+      console.log("offers:", response);
+      setOffers(response.data);
+    })
+
+    getPopularProducts().then((response) => {
+      console.log("popular:", response);
+      setPopularProducts(response.data);
     })
   }, [])
 
   const [products, setProducts] = useState([])
 
+  const [popularProducts, setPopularProducts] = useState([])
+
+  const [offers, setOffers] = useState([])
+
   return (
     <div className="bg-white flex flex-col">
       <ProductList title="Buy Now!" products={products} />
       <div id='OnSale'>
-        <ProductList title="On Sale 💯" products={products} />
+        <ProductList title="On Sale 💯" products={offers} />
       </div>
       <div id='trending'>
-        <ProductList title="Trending 🔥" products={products} />
+        <ProductList title="Trending 🔥" products={popularProducts} />
       </div>
     </div>
   );
--- src/app/product/fetchApi.js
+++ src/app/product/fetchApi.js
@@ -15,5 +15,32 @@ export const getAllProducts = async () => {
     return response;
 }
 
+export const getPopularProducts = async () => {
+    const response = await fetch(BASE_URL + "/getTopProducts",
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            console.log(response);
+            return response.json();
+        }
+        );
+    return response;
+}
 
+export const getTopOffers = async () => {
+    const response = await fetch(BASE_URL + "/getTopOffers",
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            return response.json();
+        }
+        );
+    return response;
+}
 
